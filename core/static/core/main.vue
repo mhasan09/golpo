3386cbee0f645f063c9bcb4c9dce1865509ea30a
Mahabubul Hasan <shourav6688@gmail.com>
2021-06-06 18:54:10 +0600

    changes add for post

---
+++ core/static/core/main.vue
@@ -0,0 +1,17 @@
+var Counter = {
+  data() {
+    return {
+      message: ""
+    }
+  },
+  methods: {
+    submitBid() {
+      var xx = this.message
+
+
+    },
+  },
+}
+
+
+Vue.createApp(Counter).mount('#root')
