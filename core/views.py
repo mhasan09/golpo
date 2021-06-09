e37d13312f125f1defac2a88f959b16418a19e17
Mahabubul Hasan <shourav6688@gmail.com>
2021-06-09 12:19:30 +0600

    search initiated

--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from .forms import CreateUserForm
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 from .models import POST
 
 
@@ -13,11 +14,19 @@ def feed(requests):
     for i in current_user.UserProfile.follows.all():
         user_id_list.append(i.user.id)
     posts = POST.objects.filter(created_by_id__in=user_id_list)
+    query = requests.GET.get('query', '')
+    if len(query) > 0:
+        users = User.objects.filter(username__icontains=query)
+    else:
+        users = []
     context = {
         "username": current_user,
-        "posts": posts
+        "posts": posts,
+        'query': query,
+        'users': users
     }
     return render(requests, 'core/feed.html', context)
+
 
 
 def signup(request):
