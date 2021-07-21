13106406201edd3cee47e5a51fa44f73331655b5
Mahabubul Hasan <shourav6688@gmail.com>
2021-07-21 23:31:41 +0600

    FEAT: like API half

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import POST, UserProfile
+from .models import POST, UserProfile, Like
 
 admin.site.register(POST)
 admin.site.register(UserProfile)
+admin.site.register(Like)
--- core/api.py
+++ core/api.py
@@ -7,15 +7,16 @@ from .models import POST, Like
 @login_required
 def api_POST(request):
     data = json.loads(request.body.decode('utf-8'))
-    print(data)
     post_body = data['post_body']
     POST.objects.create(body=post_body, created_by=request.user)
     return JsonResponse({'success': True})
 
+
 def api_add_likes(request):
     data = json.loads(request.body)
+    print(data)
     post_id = data['post_id']
     if not Like.objects.filter(post_id=post_id).filter(created_by=request.user).exists():
-        like = Like.objects.create(post_id=post_id,created_by=request.user)
+        like = Like.objects.create(post_id=post_id, created_by=request.user)
 
-    return JsonResponse({'success':True})
+    return JsonResponse({'success': True})
